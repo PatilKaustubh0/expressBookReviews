c4b00763945026616bb490ee23fed2c6c6b21441
PatilKaustubh0 <patilkaustubh019@gmail.com>
2024-08-07 14:20:48 +0530

    Files via Upload

--- final_project/index.js
+++ final_project/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
@@ -8,10 +9,57 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({secret:process.env.SESSION_SECRET, resave: true, saveUninitialized: true}))
 
+function getTokenRemainingTime(token) {
+    try {
+        const decoded = jwt.decode(token);
+        if (!decoded || !decoded.exp) {
+        throw new Error('Invalid token');
+        }
+
+        const currentTime = Math.floor(Date.now() / 1000);
+        const remainingTime = decoded.exp - currentTime;
+
+        if (remainingTime <= 0) {
+        return 'Token has expired';
+        }
+
+        return `Remaining time: ${remainingTime} seconds`;
+    } catch (err) {
+        return `Error: ${err.message}`;
+    }
+}
+
+
+
+// Middleware to authenticate requests to "/customer/auth" endpoint
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+    const token = req.session.token;
+
+     // Check if user is logged in and has valid access token
+     if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+
+        // Verify JWT token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+
+                console.log("Middelware :)")
+                const remainingTime = getTokenRemainingTime(token);
+                console.log(remainingTime);
+                
+                next(); // Proceed to the next middleware
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
+
 });
  
 const PORT =5000;
@@ -19,4 +67,4 @@ const PORT =5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running in PORT " + PORT));
